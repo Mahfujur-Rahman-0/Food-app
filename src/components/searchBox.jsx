e05d5e514959aa04b010e4a2e79498db46bf1c2d
Mahfujur Rahman <106644950+Mahfujur-Rahman-0@users.noreply.github.com>
2024-03-19 14:09:47 +0600

    Update searchBox.jsx

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -2,13 +2,14 @@ export default ({ value, setValue }) => {
   return (
     <>
       <div className="inputBox">
-        <input
+        <input          
           className="inputStyle"
           type="text"
           onChange={(e) => {
             setValue(e.target.value);
           }}
           value={value}
+          placeholder="Search"
         />
       </div>
     </>
